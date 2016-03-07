7ce9c2acf0ccc648360e5872ea3040223c7c77cc
Daniela <Daniela@Daniela-PC>
2016-03-07 08:25:05 -0300

    atual

--- src/TesteBanco.java
+++ src/TesteBanco.java
@@ -1,16 +1,17 @@
-
-//package br.ufs.projeto;
-
-
-
+import br.ufs.projeto.*;
+/**
+ * 
+ * @author
+ * @since
+ *
+ */
 public class TesteBanco {
-	
-
+	 
 	public static void main(String[] args) {
 
-		//Agencia agencia = new Agencia();
-		//Cliente cliente = new Cliente();
-		//Banco banco = new Banco("null", 0, null, 0);
+		Agencia agencia = new Agencia();
+		Cliente cliente = new Cliente();
+		Banco banco = new Banco("null", 0, null, 0);
 		System.out.println("MenuBanco");
 
 
